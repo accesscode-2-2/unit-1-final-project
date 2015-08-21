39a265f4ea9295e2ebecd90f506f4cb25056c883
Chrisrdavid7 <chrisrdavid7@gmail.com>
2015-08-20 20:07:17 -0400

    First Commit, madeclasses

---
+++ Time/Time/StopwatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  StopwatchViewController.h
+//  Time
+//
+//  Created by Chris David on 8/20/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StopwatchViewController : UIViewController
+
+@end
---
+++ Time/Time/TimerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimerViewController.h
+//  Time
+//
+//  Created by Chris David on 8/20/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimerViewController : UIViewController
+
+@end
