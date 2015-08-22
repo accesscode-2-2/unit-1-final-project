630fc1cb0f66a4ea6cd9bd109ef1ffb264c3d458
Eric Sze <you@example.com>
2015-08-22 11:02:37 -0400

    Configured TableView in TabController

---
+++ Time/Time/StopwatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  StopwatchTableViewController.h
+//  Time
+//
+//  Created by Eric Sze on 8/20/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StopwatchViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
