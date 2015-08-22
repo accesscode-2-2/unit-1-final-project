f3dca24d03d6f52121f88965d40badff50832cf3
Jamaal Sedayao <you@example.com>
2015-08-22 12:59:12 -0400

    working stopwatch with reset button

--- unit-1-final-project/GOStopwatchViewController.m
+++ unit-1-final-project/GOStopwatchViewController.m
@@ -1,5 +1,9 @@
 //
-//  GOStopwatchViewController.m
+//1. create bool is running so Start button can be touched once
+//2. hide reset button so can only be pressed when stopped
+//3. create bool for Lap button so cannot be pressed while paused
+//
+//GOStopwatchViewController.m
 //  unit-1-final-project
 //
 //  Created by Jamaal Sedayao on 8/21/15.
@@ -26,9 +30,12 @@
 @property (nonatomic, assign) NSTimeInterval pauseTime;
 @property (nonatomic) LapTimerTableViewCell * model;
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (nonatomic) float mainTotalTime;
+@property (nonatomic) float lapTotalTime;
+@property (nonatomic) NSDate *mainPreviousTime;
+@property (nonatomic,retain) NSDate *lapPreviousTime;
 
-@property (nonatomic) float totalTime;
-@property (nonatomic) NSDate *previousTime;
+
 
 @end
 
@@ -64,7 +71,8 @@
                                                         selector:@selector(updateMainStopwatchTimer)
                                                         userInfo:nil repeats:YES];
     
-    self.previousTime = [[NSDate alloc] init];
+    self.mainPreviousTime = [[NSDate alloc] init];
+    self.lapPreviousTime = [[NSDate alloc] init];
     
 }
 
@@ -72,27 +80,32 @@
     
     NSDate *currentTime = [[NSDate alloc] init];
     
-    NSTimeInterval ellapsedTime = [currentTime timeIntervalSinceDate: self.previousTime];
-    self.previousTime = currentTime;
+    NSTimeInterval elapsedTime = [currentTime timeIntervalSinceDate: self.mainPreviousTime];
+    self.mainPreviousTime = currentTime;
 
-    self.totalTime += ellapsedTime;
+    self.mainTotalTime += elapsedTime;
     
-    NSDate *timeDate = [NSDate dateWithTimeIntervalSince1970:self.totalTime];
+    NSDate *timeDate = [NSDate dateWithTimeIntervalSince1970:self.mainTotalTime];
     
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
-    [dateFormatter setDateFormat:@"ss "];
+    [dateFormatter setDateFormat:@"mm : ss : SS"];
     [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0.0]];
     NSString *timeString = [dateFormatter stringFromDate:timeDate];
     self.mainStopwatchLabel.text = timeString;
     
     
-    NSLog(@"%f", self.totalTime);
+    NSLog(@"%f", self.mainTotalTime);
 }
 
 - (void) updateStopwatchTimer {
-    NSDate *currentdate = [NSDate date];
-    NSTimeInterval timerInterval = [currentdate timeIntervalSinceDate:self.start];
-    NSDate *timeDate = [NSDate dateWithTimeIntervalSince1970:timerInterval];
+    NSDate *currentTime = [NSDate date];
+    NSTimeInterval elapsedTime = [currentTime timeIntervalSinceDate:self.lapPreviousTime];
+    self.lapPreviousTime = currentTime;
+    
+    self.lapTotalTime += elapsedTime;
+    
+    NSDate *timeDate = [NSDate dateWithTimeIntervalSince1970:self.lapTotalTime];
+   
     
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
     [dateFormatter setDateFormat:@"mm : ss : SS"];
@@ -106,13 +119,14 @@
     
     [self.mainStopwatch invalidate];
     [self.stopwatch invalidate];
-    self.mainStopwatch = nil;
-    self.stopwatch = nil;
+//    self.mainStopwatch = nil;
+//    self.stopwatch = nil;
 }
 
 - (IBAction)lapStopwatchButton:(UIButton *)sender {
     
-    self.start = [[NSDate alloc]init];
+    
+    self.lapTotalTime = 0.0;
     
     [self.laps addObject:self.stopwatchTimerLabel.text];
     
@@ -125,6 +139,19 @@
     self.lapNumber = ipath.row + 1;
     
     [self.numberOfLaps addObject: [NSNumber numberWithInteger:self.lapNumber]];
+}
+- (IBAction)resetStopwatchButton:(UIButton *)sender {
+   
+    [self.mainStopwatch invalidate];
+    [self.stopwatch invalidate];
+    
+    self.lapTotalTime = 0;
+    self.mainTotalTime = 0;
+    
+    self.mainStopwatchLabel.text = @"00 : 00 : 00";
+    self.stopwatchTimerLabel.text = @"00 : 00 : 00";
+
+    //[self stopStopwatchButton:sender];
 }
 
 
