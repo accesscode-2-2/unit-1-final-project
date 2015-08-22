9256d398e2be2d76565bab197bfe596f3e80d59b
Xiulan Shi <xiulanshi2013@gmail.com>
2015-08-22 11:00:43 -0400

    first commit

---
+++ Time/Time/StopWatchTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  StopWatchTabBarController.h
+//  Time
+//
+//  Created by Xiulan Shi on 8/22/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StopWatchTabBarController : UIViewController
+
+@end
---
+++ Time/Time/TimerTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TimerTabBarController.h
+//  Time
+//
+//  Created by Xiulan Shi on 8/22/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimerTabBarController : UIViewController
+
+@end
