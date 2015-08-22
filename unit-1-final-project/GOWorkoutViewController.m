441fcb5938ff4942edc6c3ddc8a081f3a2be3bfd
Fatima <fzf.villanueva@gmail.com>
2015-08-22 17:49:31 -0400

    stopwatch working and table view in timer added

---
+++ unit-1-final-project/GOWorkoutViewController.m
@@ -0,0 +1,57 @@
+//
+//  GOWorkoutViewController.m
+//  unit-1-final-project
+//
+//  Created by Fatima Zenine Villanueva on 8/22/15.
+//  Copyright © 2015 apps. All rights reserved.
+//
+
+#import "GOWorkoutViewController.h"
+
+@interface GOWorkoutViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation GOWorkoutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+
+// The TABLE
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"workoutIdentifier" forIndexPath:indexPath];
+    
+    cell.textLabel.text = @"Working MUTHHHAAFUCKAA!";
+    
+    return cell;
+}
+
+
+
+@end
