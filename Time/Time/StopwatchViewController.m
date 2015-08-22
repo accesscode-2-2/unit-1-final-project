84cb60133e00bda7e4f0d8d196ec7164539e7a6e
Mesfin <mesbekmek@gmail.com>
2015-08-22 16:23:56 -0400

    Timer Label and Lap Label functioning properly

--- Time/Time/StopwatchViewController.m
+++ Time/Time/StopwatchViewController.m
@@ -15,8 +15,14 @@
 @property (weak, nonatomic) IBOutlet UIButton *lapButton;
 @property (weak, nonatomic) IBOutlet UILabel *timerLabel;
 @property (nonatomic) CFTimeInterval initialTime;
+@property (nonatomic) CFTimeInterval lapInitialTime;
+
 
 @property (nonatomic) CADisplayLink *stopwatchTimer;
+
+@property (weak, nonatomic) IBOutlet UILabel *lapLabel;
+
+@property (nonatomic) NSMutableArray *lapTimes;
 
 @end
 
@@ -25,47 +31,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.lapTimes = [NSMutableArray new];
+    
     self.stopwatchTimer  = [CADisplayLink displayLinkWithTarget:self
                                                        selector:@selector(refreshTimerLabel)];
+    [self.stopwatchTimer setPaused:YES];
+
     [self.stopwatchTimer addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    
 }
 
--(void)refreshTimerLabel{
+-(NSString *)formatTimeString:(CFTimeInterval)timeInterval{
     CFTimeInterval currentTime = CACurrentMediaTime();
-    CFTimeInterval difference = currentTime - self.initialTime;
-
-    NSString *string = [NSString stringWithFormat:@"%02li:%02li:%02li.%03li",
-                        lround(floor(difference / 3600.)) % 100,
+    CFTimeInterval difference = currentTime - timeInterval;
+    
+    NSString *string = [NSString stringWithFormat:@"%02li:%02li.%03li",
+                        // lround(floor(difference / 3600.)) % 100,
                         lround(floor(difference / 60.)) % 60,
                         lround(floor(difference)) % 60,
                         lround(floor(difference * 1000)) % 1000];
-    
-    self.timerLabel.text = string;
-    
+    return string;
 }
 
+-(void)refreshTimerLabel{
+   
+    self.timerLabel.text = [self formatTimeString:self.initialTime];
+    self.lapLabel.text = [self formatTimeString:self.lapInitialTime];
+}
+
+
 - (IBAction)startButtonTapped:(UIButton *)sender {
-    [self.stopwatchTimer addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    self.initialTime = CACurrentMediaTime();
-    
-    
     if ([self.startButton.titleLabel.text isEqualToString:@"Start"]) {
         [self.stopwatchTimer setPaused:NO];
-        self.initialTime = CACurrentMediaTime();
+        if([self.timerLabel.text isEqualToString:@"00:00.000"]){
+            self.initialTime = CACurrentMediaTime();
+            self.lapInitialTime = self.initialTime;
+        }
         [self.startButton setTitle:@"Pause" forState:UIControlStateNormal];
+        [self.lapButton setTitle:@"Lap" forState:UIControlStateNormal];
+
+
         NSLog(@"%@", self.startButton.titleLabel.text);
     } else {
         [self.stopwatchTimer setPaused:YES];
         [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+        [self.lapButton setTitle:@"Reset" forState:UIControlStateNormal];
     }
     
 }
-
-- (IBAction)stopButtonTapped:(UIButton *)sender {
-    
-}
-
 - (IBAction)lapButtonTapped:(UIButton *)sender {
+    if([self.lapButton.titleLabel.text isEqualToString:@"Lap"]){
+        self.lapInitialTime = CACurrentMediaTime();
+        [self.lapTimes addObject:self.lapLabel.text];
+    }
+    if([self.lapButton.titleLabel.text isEqualToString:@"Reset"]){
+        [self.lapTimes removeAllObjects];
+        self.lapLabel.text = @"00:00.000";
+        self.timerLabel.text = @"00:00.000";
+    }
 }
 
 - (void)didReceiveMemoryWarning {
