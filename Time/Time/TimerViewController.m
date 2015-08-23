2d1525e135f4bce3e49cdfb5cbc509aea711443a
Eric Sanchez <ericsanchez@outlook.com>
2015-08-23 16:58:54 -0400

    grr

--- Time/Time/TimerViewController.m
+++ Time/Time/TimerViewController.m
@@ -9,29 +9,55 @@
 #import "TimerViewController.h"
 
 @interface TimerViewController ()
-
+@property (nonatomic) NSInteger countDown;
 @end
 
 @implementation TimerViewController
 
+
+
+- (void) advanceTimer:(NSTimer *)timer{
+	
+}
+
+
+- (IBAction)startButton:(id)sender {
+	
+ NSTimer *countDownTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(advanceTimer:) userInfo:nil repeats:YES];
+ 
+ NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
+ [runLoop addTimer: countDownTimer forMode:NSDefaultRunLoopMode];
+ {
+	 self.countDown = 60;
+	 
+ }
+	
+	
+}
+- (IBAction)resumeButton:(id)sender {
+}
+
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+	[super viewDidLoad];
+	//	timer = [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(currentRunLoop) userInfo:nil repeats:YES];
+	//	[[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+	
+	
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
