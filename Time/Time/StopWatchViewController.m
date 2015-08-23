d9bdc5ab5117f93a6d801da48fedaf76890d00d4
Diana <diana_elezaj@hotmail.com>
2015-08-23 15:11:27 -0400

    Lap Done

--- Time/Time/StopWatchViewController.m
+++ Time/Time/StopWatchViewController.m
@@ -14,9 +14,10 @@
 
 @property (strong, nonatomic) IBOutlet UITableView *LapTableView;
 @property (nonatomic) BOOL LapTapped;
-@property (nonatomic) BOOL firstTimeRunning;
+@property (nonatomic) BOOL SaveLappedTime;
 
 @property (nonatomic) NSMutableArray *LapArray;
+@property (nonatomic) NSMutableArray *reversedLaps;
 @property (nonatomic) void *callTheTimer;
 
 @property (strong, nonatomic) IBOutlet UILabel *StopwatchRunningLabel; //timerLabel
@@ -50,13 +51,7 @@
     
     self.LapTableView.dataSource = self;
     self.LapTableView.delegate = self;
-    
-    
-    
-    
-    
-    [self.LapArray addObject:@"90"];
-    [self.LapArray addObject:@"40"];
+    self.SaveLappedTime = NO;
     
     /******  Interface layout *****/
     [self.startStopButton.titleLabel  isEqual: @"Start"];
@@ -70,7 +65,6 @@
     self.resetLapButton.backgroundColor = [UIColor grayColor];
     self.resetLapButton.enabled = NO;
     
-   // self.LapTableView.backgroundColor = [UIColor grayColor];
     self.LapTapped = NO;
 }
 
@@ -104,6 +98,10 @@
 }
 
 
+
+
+
+
 #pragma mark - Buttons
 
 - (IBAction)startStopButtonTapped:(UIButton *)sender {
@@ -122,9 +120,6 @@
         
         // add timer to the run loop
         [[NSRunLoop currentRunLoop] addTimer:self.runningStopWatch forMode:NSRunLoopCommonModes];
-        
-        
-        
         
         
  
@@ -176,6 +171,11 @@
         self.totalSessionTime = 0;
         self.totalTime = 0;
         
+        [self.LapArray removeAllObjects];
+        [self.reversedLaps removeAllObjects];
+        [self.LapTableView reloadData];
+        
+        
         /******  Interface layout *****/
         self.StopwatchRunningLabel.text = @"0:00.0";
         self.recentLapRunning.text = @"0:00.0";
@@ -189,9 +189,13 @@
 }
   else if ([resetLapActualLabel isEqualToString:@"Lap"]) {
       self.lapStartTime = [[NSDate alloc] init];
-
-      self.LapTapped = YES;
-     [[self LapTimer] invalidate];
+      self.SaveLappedTime = YES;
+      
+      [self.LapArray addObject:self.recentLapRunning.text];
+      
+      [self.LapTableView reloadData];
+    
+      [[self LapTimer] invalidate];
       
       // setup timer
       self.LapTimer = [NSTimer timerWithTimeInterval:1/60.0 target:self selector:@selector(timerLapFired:) userInfo:nil repeats:YES];
@@ -220,76 +224,37 @@
     return self.LapArray.count;
 }
 
+
+
+
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"identifier" forIndexPath:indexPath];
-    cell.textLabel.text = self.LapArray[indexPath.row];
+    
+    
+    self.reversedLaps = [NSMutableArray arrayWithCapacity:[self.LapArray count]];
+    NSEnumerator*   reverseEnumerator = [self.LapArray reverseObjectEnumerator];
+    for (id object in reverseEnumerator)
+    {
+        [self.reversedLaps addObject:object];
+    }
+    
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Lap %ld",[self.reversedLaps count] - indexPath.row];
+    cell.detailTextLabel.text = self.reversedLaps[[self.reversedLaps count]  - 1 - indexPath.row];
+
+    
+    
+    
+//    
+//    cell.textLabel.text = [NSString stringWithFormat:@"Lap %ld",[self.LapArray count] - indexPath.row];
+//    cell.detailTextLabel.text = self.LapArray[[self.LapArray count] - 1 - indexPath.row];
+//    
     return cell;
 }
-//
-//
-//- (void) callTheTimer {
-//    
-//    //call the second timer
-//    self.runStopwatchTimer = [NSTimer timerWithTimeInterval:0.1 target:self selector:@selector(runningStopWatch:) userInfo:nil repeats:YES];
-//    
-//    [[NSRunLoop currentRunLoop] addTimer:self.runStopwatchTimer forMode:NSRunLoopCommonModes];
-//}
-//
-//
-//
-//- (void)runningStopWatch: (NSTimer *)timer{
-//    if (self.running == YES) {
-//
-//    NSTimeInterval currentTime = [NSDate timeIntervalSinceReferenceDate];
-//    NSTimeInterval elapsed = currentTime - self.totalSessionTime;
-//        self.totalTime += elapsed;
-//        
-//        self.totalSessionTime = currentTime;
-////        NSLog (@"%f currentTime now",currentTime);
-////        NSLog (@"%f elapsed now",elapsed);
-//
-//   // elapsed += self.totalTime;
-//  
-////    int mins = (int) (elapsed) / 60.0;
-////    elapsed -= mins * 60;
-////    int secs = (int) (elapsed);
-////    elapsed -=  secs;
-////    int fraction = elapsed * 10.0;
-//        
-////        NSTimeInterval seconds = fmod(self.totalTime, 60.0);
-////        NSTimeInterval minutes = floor(self.totalTime / 60.0);
-////        NSTimeInterval fraction = floor(self.totalTime / 10.0);
-//
-//        NSTimeInterval minutes = floor(self.totalTime / 60.0);
-//        NSTimeInterval seconds = fmod(self.totalTime, 60.0);
-//       // NSTimeInterval fraction = floor(self.totalTime/10 );
-//        
-//        
-//        
-////        NSLog (@"%f currentTime then",currentTime);
-////        NSLog (@"%f elapsed then",elapsed);
-//
-//        NSLog(@"%f: %f",  minutes,  seconds);
-//        //this works
-////    self.StopwatchRunningLabel.text = [NSString stringWithFormat: @"%2f%2f%2f", minutes,  seconds,  fraction];
-//        self.StopwatchRunningLabel.text = [NSString stringWithFormat: @"%02g:%02g", minutes,  seconds];
-//
-//        
-//        
-////    self.recentLapRunning.text = [NSString stringWithFormat: @"%d:%02d.%d", mins, secs, fraction];
-////    self.StopwatchRunningLabel.text = [NSString stringWithFormat: @"%d:%02d.%d", mins, secs, fraction];
-//
-//    }
-//    
-//    
-//    else if (self.running == NO) {
-//        [timer invalidate];
-//        
-//    }
-//    
-//
-//}
-//
+
+
+
 
 
 @end
