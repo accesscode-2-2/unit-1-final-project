ddd87c7a3874973035f66982f20a1da851f6d2d8
elberdev <elberdev@gmail.com>
2015-08-23 15:43:10 -0400

    cleaned up code in PresetsTableViewController

--- Time/Time/PresetsTableViewController.m
+++ Time/Time/PresetsTableViewController.m
@@ -12,6 +12,7 @@
 @interface PresetsTableViewController ()
 @property (nonatomic) OrderedDictionary *presets;
 @property (nonatomic) NSArray *selectedPreset;
+@property (nonatomic) NSUInteger chosenIndex;
 @end
 
 @implementation PresetsTableViewController
@@ -19,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupDefaultPresets];
+    [self.navigationItem setTitle:@"Select Presets"];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]
                                              initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(didSelectTimer)];
     [self.navigationItem.leftBarButtonItem setEnabled:NO];
@@ -27,36 +29,30 @@
 - (void)setupDefaultPresets {
     self.presets = [[OrderedDictionary alloc] init];
     
-    NSArray *keys = @[@"Commercials", @"Dryer",  @"Morning Meditation", @"Popcorn", @"Quick Jog", @"Washing Machine"];
-    NSArray *values = @[
-                        @[@"00", @"01", @"00"],
+    NSArray *keys = @[@"Commercials", @"Dryer",  @"Morning Meditation",
+                      @"Popcorn", @"Quick Jog", @"Washing Machine"];
+    
+    NSArray *values = @[@[@"00", @"01", @"00"],
                         @[@"00", @"45", @"00"],
                         @[@"00", @"05", @"00"],
                         @[@"00", @"03", @"30"],
                         @[@"00", @"25", @"00"],
-                        @[@"00", @"35", @"00"]
-                        ];
+                        @[@"00", @"35", @"00"]];
     
     for (int i = 0; i < [keys count]; i++) {
         [self.presets setObject:values[i] forKey:keys[i]];
     }
 }
 
-- (NSString *)formatSeconds:(CGFloat)seconds {
-    NSString *string = [NSString stringWithFormat:@"%02li:%02li:%02li",
-                        lround(floor(seconds / 3600.)) % 100,
-                        lround(floor(seconds / 60.)) % 60,
-                        lround(floor(seconds)) % 60];
-    return string;
-}
-
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [self.navigationItem.leftBarButtonItem setEnabled:YES];
+    if (self.navigationItem.leftBarButtonItem.enabled == NO) {
+        [self.navigationItem.leftBarButtonItem setEnabled:YES];
+    }
     self.selectedPreset = [self.presets objectForKey:[self.presets keyAtIndex:indexPath.row]];
-    [self.delegate presetTime:self.selectedPreset];
 }
 
 - (void)didSelectTimer {
+    [self.delegate presetTime:self.selectedPreset];
     [self dismissViewControllerAnimated:YES completion:^{}];
 }
 
@@ -76,19 +72,9 @@
     
     cell.textLabel.text = [NSString stringWithFormat:@"%@", [self.presets keyAtIndex:indexPath.row]];
     NSArray *presetTime = [self.presets objectForKey:[self.presets keyAtIndex:indexPath.row]];
-    
     cell.detailTextLabel.text = [NSString stringWithFormat:@"%@:%@:%@", presetTime[0], presetTime[1], presetTime[2]];
+    
     return cell;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
