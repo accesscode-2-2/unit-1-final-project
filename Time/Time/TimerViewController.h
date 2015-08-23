06e3c237cae92f014381b9fb418953e3100d6551
Diana <diana_elezaj@hotmail.com>
2015-08-23 17:49:51 -0400

    Timer stuff

--- Time/Time/TimerViewController.h
+++ Time/Time/TimerViewController.h
@@ -2,17 +2,12 @@
 //  TimerViewController.h
 //  Time
 //
-//  Created by Diana Elezaj on 8/22/15.
+//  Created by Diana Elezaj on 8/23/15.
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface TimerViewController : UIViewController<UIPickerViewDataSource, UIPickerViewDelegate>
-
-@property (strong, nonatomic) IBOutlet UIPickerView *picker;
-
-
-
+@interface TimerViewController : UIViewController
 
 @end
--- Time/Time/TimerViewController.m
+++ Time/Time/TimerViewController.m
@@ -2,7 +2,7 @@
 //  TimerViewController.m
 //  Time
 //
-//  Created by Diana Elezaj on 8/22/15.
+//  Created by Diana Elezaj on 8/23/15.
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
@@ -10,78 +10,28 @@
 
 @interface TimerViewController ()
 
-
-@property (nonatomic) NSArray *_pickerData;
-    
-
-@property (nonatomic) NSInteger countDown;
-@property (strong, nonatomic) IBOutlet UIButton *startStopButton;
-@property (strong, nonatomic) IBOutlet UIButton *pauseResume;
-
-
-
 @end
 
 @implementation TimerViewController
 
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //initialize Data
-    self._pickerData = @[@"Popcorn", @"Coffee", @"Reheat", @"Running", @"Defrost"];
-    // Connect data
-    self.picker.dataSource = self;
-    self.picker.delegate = self;
+    // Do any additional setup after loading the view.
 }
 
-
-
-//- (void) advanceTimer:(NSTimer *)timer{
-//	
-//}
-
-- (IBAction)startStopButton:(id)sender {
-    
-//    NSTimer *countDownTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(advanceTimer:) userInfo:nil repeats:YES];
-//    
-//    NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
-//    [runLoop addTimer: countDownTimer forMode:NSDefaultRunLoopMode];
-//    {
-//        self.countDown = 60;
-//        
-//    }
-
-
-}
-- (IBAction)pauseResumeButton:(id)sender {
-}
-
- 
 - (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
+/*
+#pragma mark - Navigation
 
- #pragma mark - PickerView
-// The number of columns of data
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
-{
-    return 1;
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
 }
-
-// The number of rows of data
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
-{
-    return self._pickerData.count;
-}
-
-// The data to return for the row and component (column) that's being passed in
-- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
-{
-    return self._pickerData[row];
-}
-
- 
+*/
 
 @end
