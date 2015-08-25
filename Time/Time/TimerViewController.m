c8ff4b05d907edafa9b64863f03074ff43f81fc6
elberdev <elberdev@gmail.com>
2015-08-25 19:02:23 -0400

    reformatting code

--- Time/Time/TimerViewController.m
+++ Time/Time/TimerViewController.m
@@ -56,61 +56,59 @@
         j++;
     }
 }
+
 - (IBAction)startButtonTapped:(UIButton *)sender {
     
     self.timer = [NSTimer timerWithTimeInterval:1 target:self selector:@selector(animate) userInfo:nil repeats:YES];
     [[NSRunLoop currentRunLoop]addTimer:self.timer forMode:NSRunLoopCommonModes];
 }
 
-
-
--(void)animate{
+- (void)animate {
     
     NSInteger hourIndex = [self.timerPickerView selectedRowInComponent:0];
     NSInteger minuteIndex = [self.timerPickerView selectedRowInComponent:1];
     NSInteger secondIndex = [self.timerPickerView selectedRowInComponent:2];
     
-    if( (hourIndex  == 00) && (minuteIndex % 60 == 00) && (secondIndex %60 == 00) ){
+    if ( (hourIndex  == 00) && (minuteIndex % 60 == 00) && (secondIndex % 60 == 00) ) {
         [self.timer invalidate];
-    }
-    else{
-        if( (hourIndex == 00) && (minuteIndex == 00) && (secondIndex == 00) ){
+    } else {
+        if ( (hourIndex == 00) && (minuteIndex == 00) && (secondIndex == 00) ) {
             [self.timer invalidate];
-        }
-        else if(secondIndex % 60 == 00){
+        } else if (secondIndex % 60 == 00) {
             
             self.row = secondIndex + 59;
             [self.timerPickerView selectRow:self.row inComponent:2 animated:YES];
             
-            if(minuteIndex != 00){
-                
+            if (minuteIndex != 00) {
                 self.row = minuteIndex -1;
                 [self.timerPickerView selectRow:self.row inComponent:1 animated:YES];
                 
-            }
-            else if(hourIndex != 00){
+            } else if (hourIndex != 00) {
+                
                 //hour
-                    self.row = hourIndex - 1;
-                    [self.timerPickerView selectRow:self.row inComponent:0 animated:YES];
+                self.row = hourIndex - 1;
+                [self.timerPickerView selectRow:self.row inComponent:0 animated:YES];
+                
                 //minute
-                if(minuteIndex%60 == 00){
-                    self.row = minuteIndex+59;
+                if (minuteIndex % 60 == 00) {
+                    self.row = minuteIndex + 59;
                     [self.timerPickerView selectRow:self.row inComponent:1 animated:YES];
-                }else{
-                    self.row = minuteIndex-1;
+                
+                } else {
+                    self.row = minuteIndex - 1;
                     [self.timerPickerView selectRow:self.row inComponent:1 animated:YES];
                 }
+                
                 //second
-                if(secondIndex%60 == 00){
+                if (secondIndex % 60 == 00) {
                     self.row = secondIndex +59;
                     [self.timerPickerView selectRow:self.row inComponent:2 animated:YES];
-                }else{
-                    self.row = secondIndex-1;
+                } else {
+                    self.row = secondIndex - 1;
                     [self.timerPickerView selectRow:self.row inComponent:2 animated:YES];
                 }
             }
-        }
-        else{
+        } else {
             self.row = secondIndex - 1;
             [self.timerPickerView selectRow:self.row inComponent:2 animated:YES];
         }
@@ -141,7 +139,7 @@
     [self presentViewController:presetsNC animated:YES completion:nil];
 }
 
--(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
     self.component = component;
     self.row = row;
 }
@@ -151,11 +149,11 @@
     
     int j = 0;
     int i = 0;
-    while(true){
-        if(i == 60){
-            i=0;
+    while (true) {
+        if (i == 60) {
+            i = 0;
         }
-        if(j==3600){
+        if (j == 3600) {
             break;
         }
         NSString *s = [NSString stringWithFormat:@"%02d", i];
@@ -202,7 +200,7 @@
 }
 
 #pragma mark - PresetsTableViewControllerDelegate implementation
--(void)presetTime:(NSArray *)presetTime {
+- (void)presetTime:(NSArray *)presetTime {
     self.presetTime = presetTime;
     NSLog(@"%@", self.presetTime);
 }
