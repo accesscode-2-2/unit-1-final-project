16fa710199b94c9e031802e04a179732d8249cb8
Fatima <fzf.villanueva@gmail.com>
2015-08-25 19:23:17 -0400

    adding calendar

--- unit-1-final-project/GOCalendarResults.m
+++ unit-1-final-project/GOCalendarResults.m
@@ -19,10 +19,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.date = self.scheduledDate; // save in property -- this is the start date
     
-
-    self.date = self.scheduledDate; //save in a property, this will be your start date
-    
+    // the timer that counts down the scheduled date
     [NSTimer scheduledTimerWithTimeInterval:1.0
                                      target:self
                                    selector:@selector(refreshLabel)
@@ -30,51 +29,55 @@
                                     repeats:YES];
 }
 
-
-
 -(void)refreshLabel
 {
-    NSDate *dateCountDown = [NSDate dateWithTimeIntervalSince1970:[self.date timeIntervalSince1970] - 1]; //the start-up date minus 1 sec.
+    //the start-up date minus 1 sec.
+    NSDate *dateCountDown = [NSDate dateWithTimeIntervalSince1970:[self.date timeIntervalSince1970] - 1];
+    
+    // the current date at the moment
+    NSDate *currentDate = [NSDate date];
+    
+    // the goal date formatter
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
     [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
     [dateFormatter setTimeStyle:NSDateFormatterNoStyle];
-    NSDate *currentDate = [NSDate date];
-    NSDateFormatter *formatterCurrent = [[NSDateFormatter alloc]init];
-    [formatterCurrent setDateFormat:@"D"];
-    NSDateFormatter *DayFormatter = [[NSDateFormatter alloc]init];
-    [DayFormatter setDateFormat:@"D"];
-    [DayFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0.0]];
+    
+    // the hour formatter
     NSDateFormatter *HourFormatter = [[NSDateFormatter alloc]init];
     [HourFormatter setDateFormat:@"HH"];
     [HourFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0.0]];
+    
+    // the minute formatter
     NSDateFormatter *MinuteFormatter = [[NSDateFormatter alloc]init];
     [MinuteFormatter setDateFormat:@"mm"];
     [MinuteFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0.0]];
+    
+    // the seconds formatter
     NSDateFormatter *SecondFormatter = [[NSDateFormatter alloc]init];
     [SecondFormatter setDateFormat:@"ss"];
     [SecondFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0.0]];
     
-    //self.countdownResultsLabel.text = [timeFormatter stringFromDate:dateCountDown];
-
-    
-
+    // calls the difference method and stores it in a string
     NSInteger difference = [self daysBetween:currentDate and:self.date];
     NSString *daysLeftString = @(difference).stringValue;
     
-    
+    // converts the formatter into strings
     NSString *formattedDateString = [dateFormatter stringFromDate:self.date];
     NSString *formattedHour = [HourFormatter stringFromDate:self.date];
     NSString *formattedMinute = [MinuteFormatter stringFromDate:self.date];
     NSString *formattedSeconds = [SecondFormatter stringFromDate:self.date];
     
+    // stores all the strings in one string for the output label
     NSString *newLabel = [NSString stringWithFormat:@"Goal Date %@ \n \n Time Left: \n \n Days: %@\n Hours %@\n Minutes %@\n Seconds%@", formattedDateString,daysLeftString, formattedHour, formattedMinute, formattedSeconds];
-    self.countdownResultsLabel.text = newLabel;
-    // Output for locale en_US: "formattedDateString: Jan 2, 2001".
-    self.date = dateCountDown;
     
-
+    // stores the countdown label into the new string
+    self.countdownResultsLabel.text = newLabel;
+    
+    // updates the output
+    self.date = dateCountDown;
 }
 
+// This is the method that subtracts the date from now versus the goal date
 - (NSInteger)daysBetween:(NSDate *)dt1 and:(NSDate *)dt2 {
     NSInteger unitFlags = NSDayCalendarUnit;
     NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier: NSGregorianCalendar];
