7fc25c18b71757a1b6d4d465d99ce05ddfffdf9c
elberdev <elberdev@gmail.com>
2015-08-25 21:51:36 -0400

    added tableviewcontroller for date countdown and added it to tabbarcontroller

---
+++ Time/Time/LapTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LapTableViewController.h
+//  Time
+//
+//  Created by Elber Carneiro on 8/25/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LapTableViewController : UITableViewController
+
+@end
