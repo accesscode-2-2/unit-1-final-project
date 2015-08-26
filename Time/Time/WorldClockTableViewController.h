f0680eb55dac0d2078ce1b03ab3219c1b869b1be
Diana <diana_elezaj@hotmail.com>
2015-08-26 14:04:36 -0400

    Started working on World Clock

---
+++ Time/Time/WorldClockTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WorldClockTableViewController.h
+//  Time
+//
+//  Created by Diana Elezaj on 8/26/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorldClockTableViewController : UITableViewController
+
+@end
