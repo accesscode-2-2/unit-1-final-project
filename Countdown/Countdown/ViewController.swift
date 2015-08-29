ac05ad5a33f659696b7e6413f452bc857787ed48
Derek316x <dereknetto@gmail.com>
2015-08-29 11:48:28 -0400

    stringFromTimeInterval

---
+++ Countdown/Countdown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Countdown
+//
+//  Created by Z on 8/29/15.
+//  Copyright © 2015 dereknetto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Timer Demo/Timer Demo/NSObjectExtension.swift
@@ -0,0 +1,13 @@
+//
+//  NSObjectExtension.swift
+//  Timer Demo
+//
+//  Created by Z on 8/29/15.
+//  Copyright © 2015 dereknetto. All rights reserved.
+//
+
+import UIKit
+
+class NSObjectExtension: NSObject {
+
+}
