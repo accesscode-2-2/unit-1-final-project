8133e9f3d45f845142b4fbd2c470cb8646f2921a
Eric Sanchez <ericsanchez@outlook.com>
2015-08-29 18:04:33 -0400

    Pause, Stop, Reset, Resume added, presets in progress

---
+++ Time/Time/Preset.m
@@ -0,0 +1,14 @@
+//
+//  Preset.m
+//  Time
+//
+//  Created by Eric Sanchez on 8/29/15.
+//  Copyright © 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "Preset.h"
+
+@implementation Preset
+
+
+@end
