426551fe7952ec6f161073e7e64e04debf6b774b
Derek316x <dereknetto@gmail.com>
2015-08-29 22:57:22 -0400

    countdown done

--- Countdown/Countdown/AddCountdownViewController.swift
+++ Countdown/Countdown/AddCountdownViewController.swift
@@ -8,22 +8,49 @@
 
 import UIKit
 
+protocol AddCountdownViewControllerDelegate {
+    func addCountdownViewController(viewController: AddCountdownViewController, didCreateNewCountdown targetDate:NSDate)
+}
+
 class AddCountdownViewController: UIViewController {
+    
+    var delegate: AddCountdownViewControllerDelegate?
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var targetDateLabel: UILabel!
+    @IBOutlet weak var targetTimeLabel: UILabel!
+    @IBOutlet weak var timerNameTextField: UITextField!
+    @IBOutlet weak var setCountdownButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateDateAndTimeLabel()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func updateDateAndTimeLabel(){
+        let dateAndTime = dateAndTimeStringsfromDate(datePicker.date)
+        targetDateLabel.text = (dateAndTime as NSArray).objectAtIndex(0) as? String
+        targetTimeLabel.text = (dateAndTime as NSArray).objectAtIndex(1)as? String
     }
-    */
+    
+    @IBAction func datePickerValueChanged(sender: UIDatePicker) {
+        updateDateAndTimeLabel()
+    }
+    
+    @IBAction func setCountdownButtonTapped(sender: UIButton) {
+        if datePicker.date.timeIntervalSinceNow > 0 { //makes sure that target date is in the future
+            let newTargetDate = datePicker.date
+            delegate?.addCountdownViewController(self, didCreateNewCountdown: newTargetDate)
+            self.dismissViewControllerAnimated(false, completion: nil)
+        }
+    }
+    
+    func dateAndTimeStringsfromDate(date:NSDate) -> ([String]){
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .LongStyle
+        formatter.timeStyle = .MediumStyle
+        let fullString = formatter.stringFromDate(datePicker.date)
+        return (fullString.componentsSeparatedByString(" at "))
+    }
 
 }
--- Countdown/Countdown/CountdownViewController.swift
+++ Countdown/Countdown/CountdownViewController.swift
@@ -8,36 +8,36 @@
 
 import UIKit
 
-class CountdownViewController: UIViewController {
+class CountdownViewController: UIViewController, AddCountdownViewControllerDelegate {
     
-
     @IBOutlet weak var timeToTargetDateLabel: UILabel!
-    @IBOutlet weak var datePicker: UIDatePicker!
-
-    var targetDate = NSDate()
-
+    var targetDate : NSDate?
+    var displayLink : CADisplayLink!
+    
     override func viewDidLoad() {
-    }
-
-    @IBAction func datePickerValueChanged(sender: UIDatePicker) {
-        targetDate = datePicker.date
-        updateTimeToTargetDate()
+        super.viewDidLoad()
+        displayLink = CADisplayLink(target: self, selector: Selector("updateTimeToTargetDate"))
+        displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
     }
     
     func updateTimeToTargetDate(){
-        let currentDate = NSDate()
-        let timeToTargetDate = targetDate.timeIntervalSinceDate(currentDate)
-        timeToTargetDateLabel.text = timeToTargetDate.stringFromTimeInterval(false)
+        if let setTargetDate = targetDate{
+            let currentDate = NSDate()
+            let timeToTargetDate = setTargetDate.timeIntervalSinceDate(currentDate)
+            timeToTargetDateLabel.text = timeToTargetDate.stringFromTimeInterval(false)
+        }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    //MARK: AddCountdownViewControllerDelegate method
+    func addCountdownViewController(viewController: AddCountdownViewController, didCreateNewCountdown targetDate: NSDate) {
+        self.targetDate = targetDate
+    }
+    
+    //MARK: Segue methods
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let viewController = segue.destinationViewController as? AddCountdownViewController {
+            viewController.delegate = self
+        }
     }
-    */
-
+    
 }
