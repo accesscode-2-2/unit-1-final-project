b39ad2d5ef62dc81ae10222fb42fe250e9ece223
Charles <ckangkitchen@gmail.com>
2015-09-01 14:04:45 -0400

    just added 1 comment, nothing changed

--- Time/TimerStopwatch/ViewController.m
+++ Time/TimerStopwatch/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "NSTimersForTableViews.h"
+//#import "NSTimersForTableViews.h" // what is this? can't find a viewcontroller for this
 #import "PresetTimersTableViewController.h"
 
 @interface ViewController ()
