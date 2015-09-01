2f84391fd938e4a321c700945a40fda7b6ec652e
Bereket Ghebremedhin <bereket6725@gmail.com>
2015-09-01 00:17:20 -0400

    made changes

---
+++ Time/TimerStopwatch/PresetTimersTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  PresetTimersTableViewController.h
+//  PresetTimerPracticeProject
+//
+//  Created by Bereket  on 8/29/15.
+//  Copyright (c) 2015 Bereket . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PresetTimersTableViewController : UITableViewController
+
+@property  NSString* transferredTimeString;
+
+@property NSMutableArray* arrayOfTimes;
+@end
---
+++ Time/TimerStopwatch/ViewController.m
@@ -0,0 +1,158 @@
+//
+//  ViewController.m
+//  PresetTimerPracticeProject
+//
+//  Created by Bereket  on 8/29/15.
+//  Copyright (c) 2015 Bereket . All rights reserved.
+//
+
+#import "ViewController.h"
+#import "NSTimersForTableViews.h"
+#import "PresetTimersTableViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.arrayForTimes= [[NSMutableArray alloc] init];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.timerPickerView.delegate=self;
+    self.timerPickerView.dataSource=self;
+    self.pickerViewArrayHours= @[@"0",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12",@"13",@"14",@"15",@"16",@"17",@"18",@"19",@"20",@"21",@"22",@"23",@"24"];
+    
+    self.minAndSecArray= [NSMutableArray arrayWithCapacity:60];
+    
+    for(int counter= 0; counter<=60; counter++){
+        
+        self.minAndSecArray[counter] = [NSString stringWithFormat:@"%d",counter];
+        
+        
+    }
+    
+    
+    self.myStoredTimeFromPickerView= [[NSMutableArray alloc] initWithObjects:@00,@00,@00, nil];
+}
+
+//returns the number of pickers
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 3;
+}
+
+
+//return the number of picker rows in each picker
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    
+    if (component == 0) {
+        return self.pickerViewArrayHours.count;
+    }
+    else if(component == 1) {
+        return 60;
+    }
+    else if(component == 2){
+        return 60;
+    }
+    
+    else {
+        NSLog(@"WTH? :/ ");
+        return 0;
+    }
+    
+    
+    
+    
+}
+
+-(NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    if (component==0){
+        
+        return self.pickerViewArrayHours[row];
+    }
+    else{
+        
+        return self.minAndSecArray[row];
+    }
+}
+
+
+- (IBAction)storeTimeFromPickerInAnArray:(id)sender {
+    
+    
+    
+    
+    
+    
+    
+    NSInteger selectedHourComponent = [self.timerPickerView selectedRowInComponent:0];
+    NSString *hourString = self.pickerViewArrayHours[selectedHourComponent];
+    
+    NSInteger selectedMinuteComponent = [self.timerPickerView selectedRowInComponent:1];
+    NSString *minuteString = self.minAndSecArray[selectedMinuteComponent];
+    
+    NSInteger selectedSecondComponent = [self.timerPickerView selectedRowInComponent:2];
+    NSString *secondString = self.minAndSecArray[selectedSecondComponent];
+    
+    self.timeString = [NSString stringWithFormat:@"%@:%@:%@", hourString, minuteString, secondString];
+    
+    
+    
+    
+    NSLog(@"%@", self.timeString);
+}
+
+-(void)trackCountdown{
+    
+}
+
+
+
+
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    
+    
+    
+    if ([[segue identifier] isEqualToString: @"segueToTableViewIdentifier"]) {
+        [self.arrayForTimes addObject:self.timeString];
+        PresetTimersTableViewController *tableViewController = (PresetTimersTableViewController *)[segue destinationViewController];
+        tableViewController.arrayOfTimes = self.arrayForTimes;
+        
+        
+        NSLog(@"%@", tableViewController.transferredTimeString);
+    }
+    
+}
+//
+//    PresetTimersTableViewController* PresetTimersTableViewControllerInstance= [[PresetTimersTableViewController alloc] init];
+
+//
+//    PresetTimersTableViewControllerInstance.transferredTimeString=self.timeString;
+//    PresetTimersTableViewController *PresetTimersViewController = (PresetTimersTableViewController *)[segue destinationViewController];
+//
+
+// Get the new view controller using [segue destinationViewController].
+// Pass the selected object to the new view controller.
+
+
+
+
+//- (NSInteger)selectedRowInComponent:(NSInteger) component{
+//
+//    return component;
+//}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
