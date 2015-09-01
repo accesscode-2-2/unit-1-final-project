91cf5c3db81ea2f2634d11614d5286afa2bd7d23
Bereket Ghebremedhin <bereket6725@gmail.com>
2015-09-01 16:12:46 -0400

    made stopWatch improvements

--- Time/TimerStopwatch/StopWatchViewController.h
+++ Time/TimerStopwatch/StopWatchViewController.h
@@ -1,45 +1,42 @@
-//
-//  ViewController.h
-//  BereketsStopWatch2.0
-//
-//  Created by Bereket  on 8/25/15.
-//  Copyright (c) 2015 Bereket . All rights reserved.
-//
+
+
+
+
+
+
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
-@property (strong, nonatomic) IBOutlet UILabel *TimeLabel; //label that shows the time
+@interface StopWatchViewController : UIViewController <UITableViewDelegate, UITableViewDataSource> {
+    
+}
 
-@property (strong, nonatomic) IBOutlet UITableView *lapsTableView; //table View that holds current laps
+//---------------------------------------------------------------------------------
+@property (strong, nonatomic) IBOutlet UIButton *startTime;
+
+@property (strong, nonatomic) IBOutlet UIButton *pauseTime;
+@property (strong, nonatomic) IBOutlet UIButton *resetTime;
+
+@property (strong, nonatomic) IBOutlet UIButton *addLapToTableView;
+
+//---------------------------------------------------------------------------------
 
 @property (nonatomic) NSTimer* timer;
-@property (nonatomic) NSDate* startTime;
+@property (nonatomic) NSDate*  beginTime;
+
 
 @property (nonatomic) NSTimeInterval totalSessonTime;  //records the time between each "Start and "Pause."
 @property (nonatomic) NSTimeInterval totalTime;        //adds the total time between all session times.
 
 
-//---------------------------------------------------------------------------
-//The properties below are tied to the buttons on the stopwatch
-@property (strong, nonatomic) IBOutlet UIButton *StartTimer;
-@property (strong, nonatomic) IBOutlet UIButton *PauseTimer;
-@property (strong, nonatomic) IBOutlet UIButton *ResetTimer;
-@property (strong, nonatomic) IBOutlet UIButton *AddLappToTableView;
-//----------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------------
 
+@property (nonatomic, strong) NSMutableArray* heldLapsForTableView;
 
-//---------------------------------------------------------------------------
+@property (strong, nonatomic) IBOutlet UILabel *timeLabel;
 
-//Mutable array to store laps when "Add Lap" button is pushed.
-@property (nonatomic, strong) NSMutableArray* LapsArray;
-
-//---------------------------------------------------------------------------
-
-
-
+@property (strong, nonatomic) IBOutlet UITableView *lapsTableView;
 
 @end
-
--- Time/TimerStopwatch/StopWatchViewController.m
+++ Time/TimerStopwatch/StopWatchViewController.m
@@ -1,30 +1,23 @@
-//
-//  ViewController.m
-//  BereketsStopWatch2.0
-//
-//  Created by Bereket  on 8/25/15.
-//  Copyright (c) 2015 Bereket . All rights reserved.
-//
+
+
+
+
+
+
 
 #import "StopWatchViewController.h"
 
-@interface ViewController ()
+@interface StopWatchViewController ()
 
 @end
 
-@implementation ViewController
+@implementation StopWatchViewController
 
 
-
-- (void)viewDidLoad {
+-(void)viewDidLoad{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    self.LapsArray = [[NSMutableArray alloc] init];
-    
-    
+    self.heldLapsForTableView = [[NSMutableArray alloc] init];
 }
-
-
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     
@@ -33,74 +26,54 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     
-    return [self.LapsArray count];
+    return self.heldLapsForTableView.count;
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"lapsCellIdentifier" forIndexPath:(indexPath)];
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath    {
     
-    cell.textLabel.text=@"bananas";
-    return cell;
+    NSString* lapsCell= @"lapsIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:lapsCell forIndexPath:(indexPath)];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", self.heldLapsForTableView[indexPath.row]];
 }
 
-//--------------------------------------------------------------------------------------------------------
 
-//-----The method below holds a series of conditionals that dictate the functions of our 4 buttons-----
 - (IBAction)buttonTapped:(id)sender {
-    
-    if(sender == self.StartTimer){
+    if (sender == self.startTime){
+        self.beginTime = [[NSDate alloc] init];
         
-        
-        //starts recording time (t=0 at this point)
-        self.startTime =[[NSDate alloc] init];
-        
-        //starts the timer with its interval
         self.timer=[NSTimer timerWithTimeInterval:1/60.0 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
         
         //adds timer to run loop
         [[NSRunLoop currentRunLoop] addTimer:self.timer forMode: NSRunLoopCommonModes];
     }
-    else if (sender == self.PauseTimer){
-        
+    else if(sender == self.pauseTime){
         self.totalTime =  self.totalTime + self.totalSessonTime;
         [self.timer invalidate];
-        
     }
-    else if (sender == self.ResetTimer){
-        
+    else if(sender == self.resetTime){
         [self.timer invalidate];
-        self.startTime= [[NSDate alloc] init];
+        self.beginTime= [[NSDate alloc] init];
         self.totalTime=00.00;
-        self.TimeLabel.text= @"00.00";
+        self.timeLabel.text= @"00.00";
         
-    }
-    else if (sender == self.AddLappToTableView){
-        
-        [self.LapsArray addObject:@2.20];
+        [self.heldLapsForTableView  removeAllObjects];
         [self.lapsTableView reloadData];
         
     }
-}
-//----------------------------END OF BUTTON CONTROL METHOD-----------------------------------------------
 
-- (void)timerFired:(NSTimer *)timer {
+    else if (sender == self.addLapToTableView){
+        [self.heldLapsForTableView addObject:self.timeLabel.text];
+        [self.lapsTableView reloadData];
+    }
     
-    // get the current time
-    NSDate *now = [[NSDate alloc] init];
+- (void)timerFired:(NSTimer*)timer {
+        
+    }
     
-    self.totalSessonTime = [now timeIntervalSinceDate:self.startTime];
-    NSTimeInterval distance =  self.totalTime + self.totalSessonTime;
-    
-    self.TimeLabel.text = [NSString stringWithFormat:@"%0.2f", distance];
-    NSLog(@"%f", distance);
-}
-//--------------------------------------------------------------------------------------------------------
-
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+    - (void)didReceiveMemoryWarning {
+        [super didReceiveMemoryWarning];
+        // Dispose of any resources that can be recreated.
+    }
 
 @end
